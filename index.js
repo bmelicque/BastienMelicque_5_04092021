0a093491c8b21772ca7a3bda4754fd154db4fa22
Bastien Mélicque <bmelicque@gmail.com>
2021-09-06 18:16:34 +0200

    Products: fetch and display

---
+++ index.js
@@ -0,0 +1,30 @@
+let cameraList = [];
+
+const fetchCameras = async () => {
+    await fetch('http://localhost:3000/api/cameras')
+    .then(res => res.json())
+    .then(data => cameraList = data);
+}
+
+const displayCameras = async () => {
+    await fetchCameras();
+
+    console.log(cameraList);
+
+    document.getElementById("list").innerHTML = cameraList.map(camera => {
+        let price = (camera.price / 100).toFixed(2) + ' €';
+        let url = "./views/product.html?id=" + camera._id;
+        return `
+        <li>
+            <a class="card" href=${url}>
+                <img class="card__img" src=${camera.imageUrl}>
+                <div class="card__content">
+                    <h2 class="card__name">${camera.name}</h2>
+                    <p class="card__price">${price}</p>
+                </div>
+            </a>
+        </li>
+        `}).join("");
+}
+
+displayCameras();
