097dc2453cd127fd0c1e9e6ff10c9043af90b4d2
Bastien Mélicque <bmelicque@gmail.com>
2021-09-07 14:06:29 +0200

    Homepage display template

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const displayCameras = async () => {
 
     console.log(cameraList);
 
+    /*
     document.getElementById("list").innerHTML = cameraList.map(camera => {
         let price = formatPrice(camera.price);
         let url = "./views/product.html?id=" + camera._id;
@@ -24,7 +25,26 @@ const displayCameras = async () => {
                 </div>
             </a>
         </li>
-        `}).join("");
+        `}).join(""); */
+
+    const template = document.getElementById("product");
+    const list = document.getElementById("list");
+
+    cameraList.forEach(camera => {
+        let clone =  document.importNode(template.content, true);
+        let url = "./views/product.html?id=" + camera._id;
+        clone.querySelector(".card").setAttribute("href", url);
+        clone.querySelector(".card__img").setAttribute("src", camera.imageUrl);
+        clone.querySelector(".card__name").textContent = camera.name;
+        clone.querySelector(".card__price").textContent = formatPrice(camera.price);
+        if (clone.querySelector(".card__description")) {
+            clone.querySelector(".card__description").textContent = camera.description;
+        }
+        
+        list.appendChild(clone);
+    });
+
+    console.log(template);
 }
 
 displayCameras();
