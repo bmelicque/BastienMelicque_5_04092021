f88725e1453dcfc389851ac3946b59f2697cbefa
Bastien Mélicque <bmelicque@gmail.com>
2021-09-08 18:08:55 +0200

    Form: error display

--- models/cart.js
+++ models/cart.js
@@ -7,21 +7,40 @@ const displayCartContent = () => {
     totalPrice.textContent = formatPrice(cart.totalPrice);
 }
 
+const displayError = source => {
+    const errors = {
+        "lastName": "Veuillez renseigner un nom correct",
+        "firstName": "Veuillez renseigner un prénom correct",
+        "address": "Veuillez renseigner une adresse correcte",
+        "city": "Veuillez renseigner un nom de ville correct",
+        "mail": "Veuillez renseigner une adresse e-mail correcte",
+    };
+    source.value = "";
+    if (document.querySelector("#"+source.id+" + .form__error") == null){
+        let errorMessage = document.createElement('p');
+        errorMessage.textContent = errors[source.name];
+        errorMessage.classList.add("form__error")
+        source.after(errorMessage);
+    }
+}
+
 const checkForm = form => {
+    let isValid = true;
+    const inputs = form.querySelectorAll('input');
     const constraints = {
         "name": new RegExp(`^[A-Za-z' -]+$`),
         "mail": new RegExp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'),
         "address": new RegExp(`^[0-9A-Za-z' -,]+$`)
     }
 
-    const inputs = form.querySelectorAll('input');
     inputs.forEach(input => {
         if (!constraints[input.dataset.type].test(input.value)) {
-            // console.log("Erreur !");
-            // Ajouter message d'erreur qui apparaît dans le formulaire
-            return false
+            displayError(input);
+            isValid = false;
         }
     })
+
+    return isValid;
 }
 
 displayCartContent();
@@ -29,6 +48,6 @@ displayCartContent();
 document.getElementById('order').addEventListener('submit', e => {
     e.preventDefault();
     if (checkForm(e.target)) {
-        // Déclencher le POST
+        
     }
 });
