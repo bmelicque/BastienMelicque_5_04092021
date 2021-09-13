2edc07a26a9f11cd566492753a6bf7bfd86e0c04
Bastien Mélicque <bmelicque@gmail.com>
2021-09-13 17:32:57 +0200

    Re-organising work folder

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+/* FUNCTIONS COMMON TO ALL/MOST PAGES */
+
 // Formats prices into displayable strings
 const formatPrice = price => {
     return (price / 100).toFixed(2).toString().replace(".", ",") + " €";
