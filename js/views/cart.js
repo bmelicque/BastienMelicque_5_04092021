f2026bc4dac2e684a5c9113cd270dfe3a6b0efb5
Bastien Mélicque <bmelicque@gmail.com>
2021-09-14 12:54:34 +0200

    Form (SASS)

--- js/views/cart.js
+++ js/views/cart.js
@@ -2,18 +2,18 @@
 // Displays an error message on the field passed in argument
 const displayError = element => {
     element.value = "";
-    if (document.querySelector("#"+element.id+" + .form__error") == null){
+    if (document.querySelector("#"+element.id+" + .contact-form__error") == null){
         const errors = {
-            "lastName": "Veuillez renseigner un nom correct",
-            "firstName": "Veuillez renseigner un prénom correct",
-            "address": "Veuillez renseigner une adresse correcte",
-            "city": "Veuillez renseigner un nom de ville correct",
+            "lastName": "Veuillez renseigner un nom correct (sans chiffres ni caractères spéciaux)",
+            "firstName": "Veuillez renseigner un prénom correct (sans chiffres ni caractères spéciaux)",
+            "address": "Veuillez renseigner une adresse correcte (sans caractères spéciaux)",
+            "city": "Veuillez renseigner un nom de ville correct (sans chiffres ni caractères spéciaux)",
             "email": "Veuillez renseigner une adresse e-mail correcte",
         };
         
         let errorMessage = document.createElement('p');
         errorMessage.textContent = errors[element.name];
-        errorMessage.classList.add("form__error");
+        errorMessage.classList.add("contact-form__error");
         element.after(errorMessage);
     }
 }
@@ -23,9 +23,9 @@ const checkForm = form => {
     let isValid = true;
     const inputs = form.querySelectorAll('input');
     const constraints = {
-        "name": new RegExp(`^[A-Za-z' -]+$`),
+        "name": new RegExp(`^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$`),
         "email": new RegExp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'),
-        "address": new RegExp(`^[0-9A-Za-z' -,]+$`)
+        "address": new RegExp(`^[0-9A-Za-zÀ-ÖØ-öø-ÿ' -,]+$`)
     }
     
     inputs.forEach(input => {
@@ -54,15 +54,18 @@ const postOrder = async (contact, products) => {
     }
     fetch('http://localhost:3000/api/cameras/order', params)
     .then(res => res.json())
-    .then(res => window.location = `./order-status.html?id=${res.orderId}&price=${cart.totalPrice}`)
+    .then(res => window.location = `./views/order-status.html?id=${res.orderId}&price=${cart.totalPrice}`)
 }
 
 /* HYDRATING THE PAGE */
 let cart = new Cart();
 cart.load();
-hydrate(document.querySelector('section'), cart.content);
 document.getElementById('cart-preview').textContent = cart.quantity;
-document.getElementById('total-price').textContent = formatPrice(cart.totalPrice);
+if (cart.quantity) {
+    hydrate(document.querySelector('section'), cart.content);
+    document.getElementById('total-price').textContent = formatPrice(cart.totalPrice);
+}
+else document.querySelector('.main').innerHTML = `<h1 class="main__title">Votre panier est vide</h1>`;
 
 // Delete buttons on each cart item
 document.querySelectorAll('.cart-item__remove').forEach((button, index) => {
@@ -75,7 +78,7 @@ document.querySelectorAll('.cart-item__remove').forEach((button, index) => {
 })
 
 // On form submit: checks user input in the form, then posts the order to the backend
-document.getElementById('order').addEventListener('submit', e => {
+document.getElementById('contact-form').addEventListener('submit', e => {
     e.preventDefault();
     if (checkForm(e.target)) {
         let contact = {};
